fed4abead983f6c5eb995821960903a03f931180
this-amazing-amy <ameliaheydorn@gmail.com>
2025-12-18 11:31:22 +0100

    Add CI pipeline to build companion app

--- @fanslib/apps/electron-companion/electron.vite.config.ts
+++ @fanslib/apps/electron-companion/electron.vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [
+      externalizeDepsPlugin({
+        exclude: ['clip-filepaths', 'clip-filepaths-win32-x64-msvc'],
+      }),
+    ],
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
